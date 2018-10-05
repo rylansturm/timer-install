24a95862fff52dfa2b45c352d1cee8493a2e89b3
RylanSturm <sturm.rw@gmail.com>
2018-10-05 10:56:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# timer-install" 
+"# This repository contains shell scripts for installing the TaktTimer app" 
+"# clone this repository, then execute the proper install script" 
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# updates Raspberry Pi and makes a working version of the TaktTimer app
+
+echo "Running Update..."
+
+# install dependencies
+sudo apt-get update
+sudo apt-get -y upgrade
+echo
+
+sudo apt-get -y install xscreensaver 
+# TODO: add command to edit xscreensaver file to disable screensaver
+# TODO: add other dependencies that I'm not thinking of now
+
+echo
+# Downloading and installing TaktTimer
+echo "Installing TaktTimer app..."
+cd ~/
+mkdir TaktTimer
+cd TaktTimer
+git init
+git remote add origin https://github.com/rylansturm/TaktTimer
+git pull origin master
+
+# 
+echo
+echo "Making desktop shortcuts..."
+chmod -x add_shortcuts.sh
+bash add_shortcuts.sh
+chmod -x ~/timer_update.sh
+cd ~/
+
+# Everything is ready! Let's run it.
+echo "Running TaktTimer..."
+python3 TaktTimer/Timer.py
